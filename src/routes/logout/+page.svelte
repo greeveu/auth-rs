eac4e3c23b927b150018582fa68b9e589f0fa27f
Tim Lohrer <me@timlohrer.de>
2025-02-17 14:46:49 +0100

    feat: add logout

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import AuthStateManager from "$lib/auth";
+    import { onMount } from "svelte";
+
+    const authStateManager = new AuthStateManager();
+
+    onMount(() => {
+        authStateManager.clearToken();
+    })
+</script>
+
+<div class="flex items-center justify-center">
+    <h1>Successfully logged out!</h1>
+</div>
