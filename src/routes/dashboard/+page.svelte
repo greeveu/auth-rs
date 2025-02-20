8249f6cd4a95350cd5cd460bbb0c6e46e763a3a9
Tim Lohrer <me@timlohrer.de>
2025-02-20 21:44:33 +0100

    feat (frontend): start profile implementation

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Profile from './profile.svelte';
 	import AuthRsApi from "$lib/api";
 	import AuthStateManager from "$lib/auth";
 	import { ClipboardList, CodeXml, Link, User } from "lucide-svelte";
@@ -36,15 +37,15 @@
 </script>
 
 <div class="flex w-screen h-screen items-center justify-center">
-    <div class="flex flex-row items-center h-[80%] w-[70%] border-[2.5px] rounded-md" style="border-color: #333; padding: 10px;">
+    <div class="flex flex-row items-center h-[80%] w-[70%] border-[2.5px] border-[#333] rounded-md" style="padding: 10px;">
         <div class="flex flex-col gap-[15px] h-[90%]">
             {#each TABS.filter(t => t.requiredRoleId ? user?.roles.includes(t.requiredRoleId) : true) as tab, index}
                 <!-- svelte-ignore a11y_no_static_element_interactions -->
                 <!-- svelte-ignore a11y_click_events_have_key_events -->
                 <div
-                    class="flex flex-row items-center justify-start gap-[15px] w-[275px] bg-red-200 h-[40px] rounded-md"
+                    class="flex flex-row items-center justify-start gap-[15px] w-[275px] border-[2px] border-[#222] rounded-md cursor-pointer hover:text-blue-500 hover:border-blue-500 transition-all"
                     class:active={currentTabIndex == index}
-                    style="padding: 5px 10px;"
+                    style="padding: 10px 15px;"
                     on:click={() => currentTabIndex = index}
                 >
                     {#if tab.icon == 'user'}
@@ -60,6 +61,24 @@
                 </div>
             {/each}
         </div>
-        <hr class="w-[2px] h-[95%] bg-[#333] rounded-[2px]" style="margin: 0 15px;">
+        <!-- svelte-ignore element_invalid_self_closing_tag -->
+        <div class="w-[4px] h-[90%] bg-[#333] rounded-[2px]" style="margin: 0 15px;" />
+        {#if user && roles}
+            {#if currentTabIndex == 0}
+                <Profile bind:user={user!} bind:roles />
+            {/if}
+        {/if}
     </div>
 </div>
+
+<style>
+    .active {
+        background-color: var(--color-blue-500);
+        border-color: var(--color-blue-500);
+    }
+
+    .active:hover {
+        color: white;
+        cursor: default;
+    }
+</style>
---
+++ src/routes/dashboard/profile.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Pencil } from "lucide-svelte";
+
+    export let user: UserMinimal;
+    export let roles: Role[];
+</script>
+
+<div class="flex flex-col items-start justify-start h-[90%] w-full gap-[20px]" style="padding-top: 0%;">
+    <div>
+        <p class="text-[14px]">> Dashboard > Your Profile</p>
+    </div>
+    <div
+        class="name-input flex justify-between items-center outline-none border-[2px] border-[#222] hover:text-blue-500 rounded-md min-w-[275px] cursor-pointer transition-all"
+        style="padding: 3.5px 10px;"
+    >
+        <p style="color: white !important;">{user.firstName} {user.lastName}</p>
+        <Pencil size="17.5" />
+    </div>
+    <div
+        class="name-input flex justify-between items-center outline-none border-[2px] border-[#222] hover:text-blue-500 rounded-md min-w-[275px] cursor-pointer transition-all"
+        style="padding: 3.5px 10px;"
+    >
+        <p style="color: white !important;">{user.email}</p>
+        <Pencil size="17.5" />
+    </div>
+    <div
+        class="name-input flex justify-between items-center outline-none border-[2px] border-[#222] rounded-md min-w-[275px]"
+        style="padding: 3.5px 10px;"
+    >
+        <p>{`${user.createdAt.split(' ')[0].split('-').reverse().join('.')} ${user.createdAt.split(' ')[1].split('.')[0]}`}</p>
+    </div>
+</div>
