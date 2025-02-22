21893d829eefefb01dc6615c6a1fb1a6761deab7
Tim Lohrer <me@timlohrer.de>
2025-02-22 19:12:18 +0100

    feat (frontend): move text field into component

---
+++ src/lib/components/dashboard/TextField.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Pencil } from "lucide-svelte";
+
+    export let label: string;
+    export let value: string;
+    export let readonly: boolean = false;
+    export let onClick: () => void = () => {};
+</script>
+
+<div class="flex flex-col">
+    <p class="text-[14px]" style="padding: 2px 6px;">{label}</p>
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div
+        class="name-input flex justify-between items-center outline-none border-[2px] border-[#222] hover:text-blue-500 rounded-md min-w-[275px] transition-all {  readonly ? 'cursor-default' : 'cursor-pointer' }"
+        style="padding: 3.5px 10px;"
+        on:click={onClick}
+    >
+        <p style="color: white !important;">{value}</p>
+        {#if !readonly}
+            <Pencil size="17.5" on:click={onClick} />
+        {/if}
+    </div>
+</div>
--- src/routes/dashboard/profile.svelte
+++ src/routes/dashboard/profile.svelte
@@ -1,32 +1,13 @@
 <script lang="ts">
-	import { Pencil } from "lucide-svelte";
+	import TextField from "$lib/components/dashboard/TextField.svelte";
+import { Pencil } from "lucide-svelte";
 
     export let user: UserMinimal;
     export let roles: Role[];
 </script>
 
-<div class="flex flex-col items-start justify-start h-[90%] w-full gap-[20px]" style="padding-top: 0%;">
-    <div>
-        <p class="text-[14px]">> Dashboard > Your Profile</p>
-    </div>
-    <div
-        class="name-input flex justify-between items-center outline-none border-[2px] border-[#222] hover:text-blue-500 rounded-md min-w-[275px] cursor-pointer transition-all"
-        style="padding: 3.5px 10px;"
-    >
-        <p style="color: white !important;">{user.firstName} {user.lastName}</p>
-        <Pencil size="17.5" />
-    </div>
-    <div
-        class="name-input flex justify-between items-center outline-none border-[2px] border-[#222] hover:text-blue-500 rounded-md min-w-[275px] cursor-pointer transition-all"
-        style="padding: 3.5px 10px;"
-    >
-        <p style="color: white !important;">{user.email}</p>
-        <Pencil size="17.5" />
-    </div>
-    <div
-        class="name-input flex justify-between items-center outline-none border-[2px] border-[#222] rounded-md min-w-[275px]"
-        style="padding: 3.5px 10px;"
-    >
-        <p>{`${user.createdAt.split(' ')[0].split('-').reverse().join('.')} ${user.createdAt.split(' ')[1].split('.')[0]}`}</p>
-    </div>
+<div class="flex flex-col items-start justify-start h-[100%] w-full gap-[10px]" style="padding-top: 0%;">
+    <TextField label="Full Name" value={`${user.firstName} ${user.lastName}`} />
+    <TextField label="Email" value={user.email} />
+    <TextField label="Creation Date" value={`${user.createdAt.split(' ')[0].split('-').reverse().join('.')} ${user.createdAt.split(' ')[1].split('.')[0]}`} readonly />
 </div>
