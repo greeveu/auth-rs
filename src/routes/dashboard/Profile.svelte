d26be24d3b82883cc4d0b290d9fda2d570a6fc85
Tim Lohrer <me@timlohrer.de>
2025-03-27 13:13:40 +0100

    fix (frontend): make system user roles readonly

--- src/routes/dashboard/Profile.svelte
+++ src/routes/dashboard/Profile.svelte
@@ -58,6 +58,6 @@
 <div class="flex flex-col items-start justify-start h-[100%] w-full gap-[10px]">
     <TextField label="Full Name" value={`${user.firstName} ${user.lastName}`} readonly={UserMinimal.isSystemAdmin(user)} />
     <TextField label="Email" value={user.email} readonly={UserMinimal.isSystemAdmin(user)} />
-    <RoleList label="Roles" roles={roles.filter(r => user.roles.includes(r._id))} onAdd={addRole} onRemove={removeRole} readOnly={!UserMinimal.isAdmin(user)} isSystemAdmin={UserMinimal.isSystemAdmin(user)} />
+    <RoleList label="Roles" roles={roles.filter(r => user.roles.includes(r._id))} onAdd={addRole} onRemove={removeRole} readOnly={!UserMinimal.isAdmin(user) || UserMinimal.isSystemAdmin(user)} isSystemAdmin={UserMinimal.isSystemAdmin(user)} />
     <TextField label="Creation Date" value={`${UserMinimal.getCreatedAt(user).getDate()}.${UserMinimal.getCreatedAt(user).getMonth() + 1}.${UserMinimal.getCreatedAt(user).getFullYear()} ${UserMinimal.getCreatedAt(user).getHours()}:${UserMinimal.getCreatedAt(user).getMinutes()}:${UserMinimal.getCreatedAt(user).getSeconds()}`} readonly />
 </div>
