125c9991a2303a13cfff6579d517ab844be5dd41
Tim Lohrer <me@timlohrer.de>
2025-04-17 02:58:36 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import AuthStateManager from "$lib/auth";
+	import { apiUrl } from "$lib/store/config";
 	import { onMount } from "svelte";
 
     onMount(async () => {
-        const loadData = await new AuthStateManager().handlePageLoad();
+        const loadData = await new AuthStateManager($apiUrl).handlePageLoad();
         if (loadData && loadData.length > 1) {
             goto('/dashboard');
         }
