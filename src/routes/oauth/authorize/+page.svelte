eec6ad6be16ec660a3dd79e7f4685d1d3e7cedc0
Tim Lohrer <me@timlohrer.de>
2025-04-17 12:43:09 +0200

    feat (frontend): display error on system user auth authorize

--- src/routes/oauth/authorize/+page.svelte
+++ src/routes/oauth/authorize/+page.svelte
@@ -9,10 +9,11 @@
         User as UserIcon,
         SquareArrowOutUpRight,
         Clock,
-        Lock
+        Lock,
+        CircleX
     } from "lucide-svelte";
 	import { INVALID_SCOPES } from "$lib/models/OAuthScopes";
-	import type User from "$lib/models/User";
+	import User from "$lib/models/User";
 	import OAuthApplication from "$lib/models/OAuthApplication";
 	import { apiUrl } from '$lib/store/config';
 
@@ -107,59 +108,70 @@
 
 <div class="flex items-center justify-center h-screen w-screen">
     <div class="flex flex-col items-center border-[2.5px] rounded-md" style="padding: 30px; border-color: #333;">
-        <div class="flex flex-col gap-2 items-center">
-            <div class="flex flex-row items-center justify-center gap-[10px]" style="margin-bottom: 20px;">
-                <Bot class="size-[80px]" />
-                <p class="opacity-50" style="letter-spacing: 3.5px;">....</p>
-                <Link class="size-[20px] opacity-75" style="margin-top: 5px;" />
-                <p class="opacity-50" style="letter-spacing: 3.5px;">....</p>
-                <UserIcon class="size-[80px]" />
+        {#if user && User.isSystemAdmin(user)}
+            <div class="flex flex-col items-center justify-center text-center gap-[25px] max-w-[500px]">
+                <CircleX size="150" color="var(--color-red-500)" />
+                <p>For security reasons, the system user can't authorize external OAuth Applications.</p>
+                <a
+                    class="text-blue-500 cursor-pointer opacity-75 hover:opacity-100 text-[18px] transition-all"
+                    href="/logout?redirect_uri={encodeURIComponent(currentPath ?? '/')}"
+                >&rarr; <i>Switch Account</i> &larr;</a>
             </div>
-            <h1 class="font-extrabold text-2xl" style="margin-bottom: 5px;">{oAuthApplication?.name}</h1>
-            <h2 class="opacity-50 text-[14px]">wants to access your Account.</h2>
-            <div class="flex flex-row gap-[10px] text-[12px]">
-                <p class="opacity-50">Signed in as</p>
-                <p class="opacity-85">{user?.firstName} {user?.lastName}</p>
-                <a class="text-blue-400" style="margin-left: 7.5px;" href="/logout?redirect_uri={encodeURIComponent(currentPath ?? '/')}">Not you?</a>
+        {:else}
+            <div class="flex flex-col gap-2 items-center">
+                <div class="flex flex-row items-center justify-center gap-[10px]" style="margin-bottom: 20px;">
+                    <Bot class="size-[80px]" />
+                    <p class="opacity-50" style="letter-spacing: 3.5px;">....</p>
+                    <Link class="size-[20px] opacity-75" style="margin-top: 5px;" />
+                    <p class="opacity-50" style="letter-spacing: 3.5px;">....</p>
+                    <UserIcon class="size-[80px]" />
+                </div>
+                <h1 class="font-extrabold text-2xl" style="margin-bottom: 5px;">{(oAuthApplication?.name?.length ?? 0) > 24 ? oAuthApplication?.name.substring(0, 25) + '...' : oAuthApplication?.name}</h1>
+                <h2 class="opacity-50 text-[14px]">wants to access your Account.</h2>
+                <div class="flex flex-row gap-[10px] text-[12px]">
+                    <p class="opacity-50">Signed in as</p>
+                    <p class="opacity-85">{user?.firstName} {user?.lastName}</p>
+                    <a class="text-blue-400" style="margin-left: 7.5px;" href="/logout?redirect_uri={encodeURIComponent(currentPath ?? '/')}">Not you?</a>
+                </div>
             </div>
-        </div>
-        <hr class="h-[2px] w-full bg-white opacity-25 rounded-[2px]" style="margin: 15px;" />
-        <div class="flex flex-col items-center gap-[10px] w-full" style="padding: 2.5px 17.5px;">
-            <ScopeList scopes={oAuthData.scopes} />
-        </div>
-        <hr class="h-[2px] w-full bg-white opacity-25 rounded-[2px]" style="margin: 10px;" />
-        <div class="flex flex-col items-start justify-center w-full max-w-[450px] gap-[10px]" style="padding: 2.5px 17.5px;">
-            <div class="flex flex-row items-center gap-[15px]">
-                <SquareArrowOutUpRight class="w-[20px] h-[20px] opacity-50" />
-                <p class="text-[11px]">
-                    <span class="opacity-50">Once you authorize you will be redirected to:</span>
-                    <span class="font-bold opacity-85">{oAuthData.redirectBase}</span>
-                </p>
+            <hr class="h-[2px] w-full bg-white opacity-25 rounded-[2px]" style="margin: 15px;" />
+            <div class="flex flex-col items-center gap-[10px] w-full" style="padding: 2.5px 17.5px;">
+                <ScopeList scopes={oAuthData.scopes} />
             </div>
-            <div class="flex flex-row items-center gap-[12.5px]">
-                <Clock class="w-[17.5px] h-[17.5px] opacity-50" />
-                <p class="text-[11px] opacity-85"><span class="opacity-70">Active since</span> {oAuthData.activeSince}</p>
+            <hr class="h-[2px] w-full bg-white opacity-25 rounded-[2px]" style="margin: 10px;" />
+            <div class="flex flex-col items-start justify-center w-full max-w-[450px] gap-[10px]" style="padding: 2.5px 17.5px;">
+                <div class="flex flex-row items-center gap-[15px]">
+                    <SquareArrowOutUpRight class="w-[20px] h-[20px] opacity-50" />
+                    <p class="text-[11px]">
+                        <span class="opacity-50">Once you authorize you will be redirected to:</span>
+                        <span class="font-bold opacity-85">{oAuthData.redirectBase}</span>
+                    </p>
+                </div>
+                <div class="flex flex-row items-center gap-[12.5px]">
+                    <Clock class="w-[17.5px] h-[17.5px] opacity-50" />
+                    <p class="text-[11px] opacity-85"><span class="opacity-70">Active since</span> {oAuthData.activeSince}</p>
+                </div>
+                <div class="flex flex-row items-center gap-[12.5px]">
+                    <Lock class="w-[30px] h-[30px] opacity-50" />
+                    <p class="text-[11px] opacity-70">This application will never be able to access anything outside of the permissions mentioned above.</p>
+                </div>
             </div>
-            <div class="flex flex-row items-center gap-[12.5px]">
-                <Lock class="w-[30px] h-[30px] opacity-50" />
-                <p class="text-[11px] opacity-70">This application will never be able to access anything outside of the permissions mentioned above.</p>
+            <hr class="h-[2px] w-full bg-white opacity-25 rounded-[2px]" style="margin: 10px;" />
+            <!-- svelte-ignore a11y_click_events_have_key_events -->
+            <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+            <div class="flex flex-row items-center justify-between w-full" style="margin-top: 10px;">
+                <h1
+                    class="text-red-500 cursor-pointer btn cancel"
+                    class:opacity-50={step != 0}
+                    on:click={step == 0 ? cancel : null}
+                >Cancel</h1>
+                <h1
+                    class="text-green-500 font-bold cursor-pointer btn authorize"
+                    class:opacity-50={step != 0}
+                    on:click={step == 0 ? authorize : null}
+                >{step == 0 ? 'Authorize' : 'Authorizing...'}</h1>
             </div>
-        </div>
-        <hr class="h-[2px] w-full bg-white opacity-25 rounded-[2px]" style="margin: 10px;" />
-        <!-- svelte-ignore a11y_click_events_have_key_events -->
-        <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-        <div class="flex flex-row items-center justify-between w-full" style="margin-top: 10px;">
-            <h1
-                class="text-red-500 cursor-pointer btn cancel"
-                class:opacity-50={step != 0}
-                on:click={step == 0 ? cancel : null}
-            >Cancel</h1>
-            <h1
-                class="text-green-500 font-bold cursor-pointer btn authorize"
-                class:opacity-50={step != 0}
-                on:click={step == 0 ? authorize : null}
-            >{step == 0 ? 'Authorize' : 'Authorizing...'}</h1>
-        </div>
+        {/if}
     </div>
 </div>
 
